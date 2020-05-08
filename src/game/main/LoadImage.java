d39bbc2122ff056bee03087a5becf39ae3641491
linh nguyen <jonathanrocrach2@gmail.com>
2020-05-08 19:42:52 +0700

    first commit

---
+++ src/game/main/LoadImage.java
@@ -0,0 +1,28 @@
+package game.main;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class LoadImage {
+    public static BufferedImage image;
+    public static BufferedImage entities;
+    public static BufferedImage enemy, player;
+
+    public static void init() {
+       image = imageLoader("../resources/whte.png");
+       entities = imageLoader("../resources/airplane.png");
+       enemy = entities.getSubimage(0, 0,85, 90);
+       player = entities.getSubimage(85, 0, 95, 90);
+    }
+
+    private static BufferedImage imageLoader(String path) {
+        try {
+            return ImageIO.read(LoadImage.class.getResource(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+    }
+}
